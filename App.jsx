48c2be20c5dce757e15caa1fd1a43f01f002ecfc
Navya-Manu <85414850+Navya-Manu@users.noreply.github.com>
2021-06-06 01:50:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import "../styles.css";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h2> Javascript and React.js </h2>
+      <p>
+        
+        This was an amazing bootcamp taken up by taken up by Shaurya Sinha we
+        covered everything from scratch including Javascript ,React.js,HTML
+      </p>
+    </div>
+  );
+}
+export default Note;
